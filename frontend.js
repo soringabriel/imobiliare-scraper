a6e2a6248ea1d949138f8daa667e08b680fbaaf3
SorinMarica <sorin.marica@jeco.dev>
2022-10-14 16:48:30 +0300

    Frontend implementation

---
+++ frontend.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const dotenv = require('dotenv');
+const Database = require("./database");
+dotenv.config({path: path.resolve(__dirname, '.env')})
+
+const {plot} = require('nodeplotlib');
+
+let db = new Database('aggregated');
+
+function plotGroupedData(title, fulldata, keys, mainKey) {
+    let data = [];
+    for (let index in keys) {
+        let key = keys[index];
+        data.push({
+            x: fulldata.map((x) => { return x.timestamp; }),
+            y: fulldata.map((x) => { return x[mainKey][key] ? x[mainKey][key].avg : -1; }),
+            type: 'line',
+            name: `${title} - ${key}`
+        })
+    }
+    plot(data);
+}
+
+function plotMainData(title, fulldata, key) {
+    let data = [];
+    data.push({
+        x: fulldata.map((x) => { return x.timestamp; }),
+        y: fulldata.map((x) => { return x[key].avg; }),
+        type: 'line',
+        name: `${title}`
+    })
+    plot(data);
+}
+
+(async () => {
+    let fulldata = await db.find()
+
+    plotGroupedData("Avg Price By Surface Range", fulldata, Object.keys(fulldata[0].by_surface_range), "by_surface_range");
+    plotGroupedData("Avg Price By Rooms No.", fulldata, Object.keys(fulldata[0].by_rooms), "by_rooms");
+    plotGroupedData("Avg Price By Area", fulldata, Object.keys(fulldata[0].by_area), "by_area");
+    plotMainData("Avg Price Per Total", fulldata, "total");
+
+})();
